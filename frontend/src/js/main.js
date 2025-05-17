f06ff51f0dade3cfca60f8783da220b62daf2e96
Manos <csd4281@csd.uoc.gr>
2025-05-17 19:15:07 +0300

    feat: initial homepage

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -2,3 +2,14 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../css/style.css";
+import $ from "jquery";
+
+window.addEventListener("DOMContentLoaded", () => {
+  $("#app").load("/pages/home.html");
+});
+
+$(document).on("click", "#home-btn", () => {
+  $("#app").load("/pages/home.html");
+});
