285796e12600fd26f3f45f18967f5ce28cd24314
Manos <csd4281@csd.uoc.gr>
2025-05-18 02:57:00 +0300

    feat: user registration form

--- frontend/src/js/utils.js
+++ frontend/src/js/utils.js
@@ -1,7 +1,89 @@
 "use strict";
 
+// Display registration form for a new user
 function registerUser() {
-  console.log("Registering user...");
+  $("#app").load("/pages/register-user.html", function () {
+    $("#birthdate").datepicker({
+      dateFormat: "yy-mm-dd",
+      changeYear: true,
+      changeMonth: true,
+      yearRange: "1900:" + (new Date().getFullYear() - 18),
+      defaultDate: new Date(new Date().getFullYear() - 18, 0, 1),
+    });
+  });
+}
+
+$(document).on("submit", "#registration-form", function (event) {
+  event.preventDefault();
+
+  let formData = {
+    firstName: $("#firstName").val(),
+    lastName: $("#lastName").val(),
+    gender: $("#gender").val(),
+    birthDate: $("#birthdate").val(),
+    homeAddress: $("#homeAddress").val(),
+    workAddress: $("#workAddress").val(),
+  };
+
+  if (!formData.firstName.trim()) {
+    popInfoMessage("Please enter a first name to continue!");
+    return;
+  }
+  if (!formData.lastName.trim()) {
+    popInfoMessage("Please enter a last name");
+    return;
+  }
+  if (!formData.gender) {
+    popInfoMessage("Please select a gender to continue!");
+    return;
+  }
+
+  if (!isValidDate(formData.birthDate)) {
+    popInfoMessage("Please enter a valid date in the format YYYY-MM-DD!");
+    return;
+  }
+
+  if (!formData.homeAddress.trim()) {
+    formData.homeAddress = null;
+  } else {
+    formData.homeAddress = { type: "HOME", FullAddress: formData.homeAddress };
+  }
+  if (!formData.workAddress.trim()) {
+    formData.workAddress = null;
+  } else {
+    formData.workAddress = { type: "WORK", FullAddress: formData.workAddress };
+  }
+
+  console.log("Form data: ", formData);
+
+  //all checks passed, send the data to the server
+  $.ajax({
+    url: "http://localhost:8080/api/user/add",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(formData),
+    success: function (data) {
+      console.log(data);
+      popInfoMessage("User registered successfully!", "Registration Complete");
+      $("#registration-form").trigger("reset"); // empty the form after successful submission
+    },
+    error: function (xhr) {
+      console.log(xhr);
+      popInfoMessage(
+        "Error registering user: " + xhr.responseJSON.error,
+        "Error " + xhr.status
+      );
+    },
+  });
+});
+
+// This function checks if the date string is in the format YYYY-MM-DD
+function isValidDate(dateString) {
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  if (!regex.test(dateString)) return false;
+
+  const date = new Date(dateString);
+  return !isNaN(date.getTime());
 }
 
 // Display registered users in a table
