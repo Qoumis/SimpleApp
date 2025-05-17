c4165d006aed45e92fe094681f2e7507a3b0644d
Manos <csd4281@csd.uoc.gr>
2025-05-17 21:59:34 +0300

    feat: user deletion button

--- frontend/src/js/utils.js
+++ frontend/src/js/utils.js
@@ -4,6 +4,7 @@ function registerUser() {
   console.log("Registering user...");
 }
 
+// Display registered users in a table
 function displayUsers() {
   $("#app").load("/pages/display-users.html", function () {
     $.ajax({
@@ -14,9 +15,10 @@ function displayUsers() {
         let table = $("#users-table tbody");
         data.forEach((user) => {
           table.append(
-            `<tr onclick="console.log(${user.id})">
-              <td>${user.firstName}</td>
-              <td>${user.lastName}</td>
+            `<tr>
+              <td onclick="showUserInfo(${user.id})">${user.firstName}</td>
+              <td onclick="showUserInfo(${user.id})">${user.lastName}</td>
+              <td class="text-end"><button class="btn btn-outline-danger" onclick="popDeleteConfrimation(${user.id}, '${user.firstName}', '${user.lastName}')">Delete</button></td>
             </tr>`
           );
         });
@@ -31,6 +33,43 @@ function displayUsers() {
   });
 }
 
+//Delete a user from the database
+//This function should be called when the delete button is clicked
+function deleteUser(id) {
+  $.ajax({
+    url: "http://localhost:8080/api/user/" + id,
+    type: "DELETE",
+    success: function (_msg) {
+      displayUsers(); // Refresh the user list after deletion
+    },
+    error: function (xhr) {
+      console.log(xhr);
+      popInfoMessage(
+        "Error deleting user: " + xhr.responseText,
+        "Error " + xhr.status
+      );
+    },
+  });
+}
+
+// Pop a modal to confirm deletion of a user
+function popDeleteConfrimation(id, firstName, lastName) {
+  $("#DeleteConfirmation-msg").html(
+    "Are you sure you want to delete user: " + firstName + " " + lastName + "?"
+  );
+  $("#DeleteConfirmation").modal("show");
+  $("#DeleteConfirmation-btn").off("click"); // Remove any previous click events
+  $("#DeleteConfirmation-btn").on("click", function () {
+    deleteUser(id);
+    $("#DeleteConfirmation").modal("hide");
+  });
+}
+
+function showUserInfo(id) {
+  console.log("Showing info for user with ID: " + id);
+}
+
+// Used to pop a modal with a message (mostly when an error occurs)
 function popInfoMessage(message, title = "Error") {
   $("#InfoModal-msg").html(message);
   $("#InfoModal-label").html(title);
